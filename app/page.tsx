7c0be922bd1b2ca1838d13f77b8772b725257234
Alexis-Contente <alexis.contente@gmail.com>
2023-12-05 13:40:38 +0100

    Creation d'un header et footer, d'un fichier .env et insertion de la key API

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import Footer from "@/components/footer/page";
 import styles from "./page.module.css";
+import Header from "@/components/header/page";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello world</h1>
-    </main>
+    <>
+      <Header />
+      <main className={styles.main}>
+        <h1>Hello world</h1>
+      </main>
+      <Footer />
+    </>
   );
 }
---
+++ components/header/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <h1>Pokémon</h1>
+    </header>
+  );
+}
