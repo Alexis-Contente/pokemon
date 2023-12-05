edc7bb8498601402666456b893af144cc662f545
Alexis-Contente <alexis.contente@gmail.com>
2023-12-05 14:39:37 +0100

    Création de la page de tous les pokémons, fetch all pokémons

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 import Footer from "@/components/footer/page";
 import styles from "./page.module.css";
 import Header from "@/components/header/page";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <Header />
       <main className={styles.main}>
-        <h1>Hello world</h1>
+        <h1>Bienvenue</h1>
+        <p>
+          Retrouvez tous les Pokémons de l&apos;emblématique dessin animé, ainsi
+          que toutes les infos liées à ces Pokémons !
+        </p>
+        <Link href="/pokemons">Tous les Pokémons</Link>
       </main>
       <Footer />
     </>
---
+++ app/pokemons/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import Header from "@/components/header/page";
+import styles from "./page.module.css";
+import Footer from "@/components/footer/page";
+import { useEffect, useState } from "react";
+
+export default function Pokemons() {
+  const [cards, setCards] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchCards = async () => {
+    try {
+      const response = await fetch("https://tyradex.vercel.app/api/v1/pokemon");
+      const data = await response.json();
+      console.log("Cards:", data);
+      setCards(data);
+    } catch (error) {
+      console.log("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCards();
+  }, []);
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <h1>Pokémons</h1>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          cards.map((card) => (
+            <div key={card.id}>
+              <h2>{card.name.fr}</h2>
+            </div>
+          ))
+        )}
+      </main>
+      <Footer />
+    </>
+  );
+}
