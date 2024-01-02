7a49a2f3d2b46bc0e8a4ae5986d7d86a8bbb3009
Alexis-Contente <alexis.contente@gmail.com>
2024-01-02 15:52:56 +0100

    ajout du logo dans le header

--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,11 +1,13 @@
-import Link from "next/link";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "@/public/assets/images/pokemon.svg";
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href={"/"} className={styles.title}>
-        Pokémon
+      <Link href={"/"} className={styles.link}>
+        <Image src={Logo} alt="logo" className={styles.img} />
       </Link>
     </header>
   );
